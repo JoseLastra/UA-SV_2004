5ab0af1cd6df69d0b8530196a29add1789eb41a4
JoseLastra <jose.lastra@pucv.cl>
2024-12-08 17:13:53 +0100

    add script day 1

---
+++ RScripts/01_Day1_UASV_2024.R
@@ -0,0 +1,16 @@
+# 1.- UASV 2025: Day 1 'Prob modelling and Taylor series' ----------
+##------------------------------------------------------------------------#
+## @date 2024-12-09
+## @project C:/github/UA-SV_2004
+## @R version R version 4.3.2 (2023-10-31 ucrt)
+## @OS system Windows 10 x64
+## @author Jose A. Lastra
+## @email jose.lastramunoz@wur.nl | jose.lastra@pucv.cl
+##------------------------------------------------------------------------#
+# 2.- Libraries --------
+
+## Clean environment ----
+rm(list = ls(all = T))
+
+##------------------------------------------------------------------------#
+# 3.- General Inputs -----
