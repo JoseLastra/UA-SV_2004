d1a58830c5b7b582495bb16e3d5a95962f60d062
JoseLastra <jose.lastra@pucv.cl>
2024-12-11 09:01:07 +0100

    add day 3

---
+++ RScripts/03_Day3_UASV_2024.R
@@ -0,0 +1,23 @@
+# 1.- UASV 2025: Day 2 'MONTECARLO' ----------
+## ------------------------------------------------------------------------#
+## @date 2024-12-10
+## @project C:/github/UA-SV_2004
+## @R version R version 4.3.2 (2023-10-31 ucrt)
+## @OS system Windows 10 x64
+## @author Jose A. Lastra
+## @email jose.lastramunoz@wur.nl | jose.lastra@pucv.cl
+## ------------------------------------------------------------------------#
+# 2.- Libraries --------
+pacman::p_load(tidyverse, sf, terra)
+## Clean environment ----
+rm(list = ls(all = T))
+
+## ------------------------------------------------------------------------#
+# 3.- Exercises -----
+
+
+
+
+
+
+
