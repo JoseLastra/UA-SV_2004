87cfe975b95cabb5e7dcf0850061be8a8b3ebb33
JoseLastra <jose.lastra@pucv.cl>
2024-12-11 10:44:45 +0100

    copilot answer to excercise one about probabilities

--- RScripts/03_Day3_UASV_2024.R
+++ RScripts/03_Day3_UASV_2024.R
@@ -1,4 +1,4 @@
-# 1.- UASV 2025: Day 2 'MONTECARLO' ----------
+# 1.- UASV 2025: Day 3 'Model uncertainty' ----------
 ## ------------------------------------------------------------------------#
 ## @date 2024-12-10
 ## @project C:/github/UA-SV_2004
@@ -14,6 +14,20 @@ rm(list = ls(all = T))
 
 ## ------------------------------------------------------------------------#
 # 3.- Exercises -----
+
+# Try Bayes’ law yourself (tip: make also use of the ‘Law of
+#                          Total Probability’):
+# ▪ Thunderstorms happen on average 2 out of 100 days (2%)
+# ▪ The probability of rain in case of a thunderstorm is 80%
+# ▪ The probability of rain in case of no thunderstorm is 5%
+# ▪ What is the probability of thunderstorm in case of rain?
+#Calculate the probability of thunderstorm in case of rain
+#P(Thunderstorm | Rain) = P(Rain | Thunderstorm) * P(Thunderstorm) / P(Rain)
+#P(Thunderstorm | Rain) = 0.8 * 0.02 / P(Rain)
+
+#Calculate the probability of rain
+#P(Rain) = P(Rain | Thunderstorm) * P(Thunderstorm) + P(Rain | No Thunderstorm) * P(No Thunderstorm)
+#P(Rain) = 0.8 * 0.02 + 0.05 * 0.98
 
 
 
