2de7ad371625fa0c4e3f84ace695768597cfa906
JoseLastra <jose.lastra@pucv.cl>
2024-12-12 09:04:42 +0100

    add script base for day 4

---
+++ RScripts/04_Day4_UASV_2024.R
@@ -0,0 +1,30 @@
+# 1.- UASV 2025: Day 4 'Validation' ----------
+## ------------------------------------------------------------------------#
+## @date 2024-12-12
+## @project C:/github/UA-SV_2004
+## @R version R version 4.3.2 (2023-10-31 ucrt)
+## @OS system Windows 10 x64
+## @author Jose A. Lastra
+## @email jose.lastramunoz@wur.nl | jose.lastra@pucv.cl
+## ------------------------------------------------------------------------#
+# 2.- Libraries --------
+pacman::p_load(tidyverse, sf, terra)
+## Clean environment ----
+rm(list = ls(all = T))
+
+## ------------------------------------------------------------------------#
+# 3.- Exercises -----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
